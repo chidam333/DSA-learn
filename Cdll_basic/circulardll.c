f68c8d8c66a37a09cc346e0375b552c166d10929
chidam <chidam3chain@gmail.com>
2022-06-11 17:31:22 +0530

    CDll delete and ordered input fixed

--- Cdll_basic/circulardll.c
+++ Cdll_basic/circulardll.c
@@ -46,6 +46,51 @@ node *inspos(node *first,int x,int p){
     cur->prev=t;
     return first;
 }
+node *insord(node *first,int x){
+    node *t;
+    t=(node*)malloc(sizeof(node));
+    t->x=x;
+    if(first==NULL){
+        t->next=t->prev=t;
+        first=t;
+        return first;
+    }
+    if(first->x>x){
+        t->next=first;
+        t->prev=first->prev;
+        first->prev->next=t;
+        first->prev=t;
+        first=t;
+        return first;
+    }
+    node *cur=first->next;
+    while(cur!=first && cur->x<x){
+        cur=cur->next;
+    }
+    t->next=cur;
+    t->prev=cur->prev;
+    cur->prev->next=t;
+    cur->prev=t;
+    return first;
+}
+node *deleteval(node* first,int x){
+    if(first->x==x){
+        first->next->prev=first->prev;
+        first->prev->next=first->next;
+        node *t=first;
+        first=first->next;
+        free(t);
+        return first;
+    }
+    node *t=first->next;
+    while(t!=first && x!=t->x){
+        t=t->next;
+    }
+    t->next->prev=t->prev;
+    t->prev->next=t->next;
+    free(t);
+    return first;
+}
 void display(node *first){
     if(first==NULL){
         printf("there is nothing in here");
--- Cdll_basic/main.c
+++ Cdll_basic/main.c
@@ -4,7 +4,7 @@ int main(){
     node *first=NULL;
     int op;
     do{
-        printf("\n1.Insbeg\n2.Inspos\n5.Display\n6.Break");
+        printf("\n1.Insbeg\n2.Inspos\n3.Insord\n4.Deleteval\n5.Display\n6.Break");
         scanf("%d",&op);
         switch(op){
             case 1:
@@ -21,6 +21,18 @@ int main(){
                 printf("Which position do you want to enter");
                 scanf("%d",&p);
                 first=inspos(first,z,p);
+                break;
+            case 3:
+                int y;
+                printf("The value you want to append in order");
+                scanf("%d",&y);
+                first=insord(first,y);
+                break;
+            case 4:
+                int q;
+                printf("Enter the value you want to delete :");
+                scanf("%d",&q);
+                first=deleteval(first,q);
                 break;
             case 5:
                 display(first);
